52b95bb1771ed7f403ee2a0e0272e017419bd2c7
Yavuz Selim <fbyavuzselim@gmail.com>
2026-09-02 13:43:14 +0200

    Deploy AKS cluster with custom VNet, subnet, and non-overlapping service CIDR

---
+++ infra/aks.tf
@@ -0,0 +1,29 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-gitops-platform"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksgitops"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = "Standard_D2s_v7"
+    vnet_subnet_id = azurerm_subnet.aks.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+network_profile {
+    network_plugin = "azure"
+    service_cidr   = "172.16.0.0/16"
+    dns_service_ip = "172.16.0.10"
+  }
+  
+  tags = {
+    project     = var.project_name
+    environment = "portfolio"
+    managed_by  = "terraform"
+  }
+}
--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_storage_account" "tfstate" {
   name                     = "sttfstateyavuz2026"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = "germanywestcentral"
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
